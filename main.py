8bd90f754e965eedf0035db5ef883c0bfb9c1d08
vcabrales0115 <vcabrales@cps.edu>
2026-02-10 15:35:34 +0000

    task

---
+++ main.py
@@ -0,0 +1,12 @@
+#Packing orgainizer
+#Ask how many nights are you staying on your trip?
+days_gone=input(f"How long will you be gone on your trip?")
+def days_gone():
+
+    if items_in_closet => 110
+        print("You should declutter")
+    else items_in_closet =< 100
+        print("You")
+
+
+#orgainize item in closet part of the code
