69daab7244d15e33cfaefc41878e75af24ee0765
vcabrales0115 <vcabrales@cps.edu>
2026-02-20 15:23:52 +0000

    task

--- main.py
+++ main.py
@@ -2,10 +2,34 @@
 def savings(income, costs):
     return
 
-income= input("What is your monthly income?")
+income= input("What is your monthly income? ")
 if income==ValueError:
     print("please enter a valid answer ")
-costs = []
+monthly_costs = []
 while True:
-    costs= input("What is your average costs per month?")
+    rent= input("How much is your rent this month? ")
+    utillies= input("How much are your utillies this month? ")
+    grocery= input("What is your average grocery costs per month? ")
 
+    
+    monthly_costs.append(rent)
+    monthly_costs.append(utillies)
+    monthly_costs.append(grocery)
+    print("Your_monthly_costs:", monthly_costs)
+    break
+#Add costs together
+def add (rent, utilities, grocery):
+    monthly_costs= rent + utillies + grocery
+    return monthly_costs()
+#Subtract costs from income
+def subtract (income, monthly_costs):
+    discretionary_income = income - monthly_costs
+    return discretionary_income()
+if __monthly_costs__ == "__main__":
+income == discretionary_income()
+print(discretionary_income[])
+
+
+
+# Updated_list = {rent, utillies*, miscellaneous**}
+# print(Updated_list(rent, utillies, miscellaneous))
