49dfd8c835724e84c8ba95b050b5a8b93d33bd25
vcabrales0115 <vcabrales@cps.edu>
2026-02-24 15:28:28 +0000

    task

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
 #Budget tracker
 def savings(income, costs):
-    return
+    return income - costs
 
 income= input("What is your monthly income? ")
 if income==ValueError:
     print("please enter a valid answer ")
 monthly_costs = []
 while True:
-    rent= input("How much is your rent this month? ")
-    utillies= input("How much are your utillies this month? ")
-    grocery= input("What is your average grocery costs per month? ")
-
+    rent= int(input("How much is your rent this month? "))
+    utillies= int(input("How much are your utillies this month? "))
+    grocery= int(input("What is your average grocery costs per month? "))
     
-    monthly_costs.append(rent)
-    monthly_costs.append(utillies)
-    monthly_costs.append(grocery)
-    print("Your_monthly_costs:", monthly_costs)
-    break
-#Add costs together
-def add (rent, utilities, grocery):
-    monthly_costs= rent + utilities + grocery
-    return monthly_costs
-#Subtract costs from income
-def subtract (income, monthly_costs):
-    discretionary_income = income - monthly_costs
-    return discretionary_income
+for cost in [rent, utillies, grocery]:
+    if cost < 0:
+        print("Please enter a valid answer")
+        break   
+    
 
-if __name__ == "__main__":
-    income == discretionary_income()
-print(discretionary_income,[])
+#Add costs together
+total = rent + utillies + grocery
+monthly_costs= rent + utillies + grocery
+print(f"Your monthly costs is", {total})
+if monthly_costs >0:
+    def subtract(income, monthly_costs):
+        discretionary_income = income - monthly_costs
+        print(discretionary_income)
+else:
+    print("Try again")
+
+# #Subtract costs from income
+# def subtract (income, monthly_costs):
+#     discretionary_income = income - monthly_costs
+#     discretionary_income
+
+
 
 
 
