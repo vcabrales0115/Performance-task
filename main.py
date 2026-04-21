3e42a1ee9952eea9bf3442c3845db76f7c1f1faf
vcabrales0115 <vcabrales@cps.edu>
2026-04-20 22:15:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 #Budget tracker
 
-def savings(income, costs):
-    return income - costs
-
 income= input("What is your monthly income? ")
 if income==ValueError:
     print("please enter a valid answer ")
 monthly_costs = []
+
+
+#running sequence of code using loops
+
+
 while True:
     rent= int(input("How much is your rent this month? "))
     utillies= int(input("How much are your utillies this month? "))
     grocery= int(input("What is your average grocery costs per month? "))
     break
     
+
 for cost in [rent, utillies, grocery]:
     if cost < 0:
         print("Please enter a valid answer")
         break   
-    
+    #end loop
+
 
 #Add costs together
+#list including the costs of each catagory
+
+
 monthly_costs = [rent, utillies, grocery]
 total = rent + utillies + grocery
 print(f"Your monthly costs are" ,total,"dollars")
@@ -27,4 +34,5 @@ print(f"Your monthly costs are" ,total,"dollars")
 #Subtract costs from income
 
 balence_remaining= int(income)-total
-print(f"You have a remaining balence of",balence_remaining,"dollars,this month!")
+print(f"You have a remaining balence of",balence_remaining,"dollars this month!")
+ 
