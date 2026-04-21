6fb1c8a76f918f4af04f639eb21a41a7bb89188e
vcabrales0115 <vcabrales@cps.edu>
2026-04-20 22:58:12 -0500

    Implement savings function and validate costs input
    Added a savings function to calculate remaining balance after costs.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #Budget tracker
 
+def savings(income, costs):
+    return income - costs
+
 income= input("What is your monthly income? ")
 if income==ValueError:
     print("please enter a valid answer ")
@@ -13,13 +16,16 @@ while True:
     rent= int(input("How much is your rent this month? "))
     utillies= int(input("How much are your utillies this month? "))
     grocery= int(input("What is your average grocery costs per month? "))
-    break
     
+    for cost in [rent, utillies, grocery]:
+        if cost < 0:
+            print("Please enter a valid answer")
+    again= input("Enter again?(press q to quit, enter to repeat)")
+    if again == "q":
 
-for cost in [rent, utillies, grocery]:
-    if cost < 0:
-        print("Please enter a valid answer")
-        break   
+        break
+    else:
+        continue
     #end loop
 
 
@@ -33,6 +39,5 @@ print(f"Your monthly costs are" ,total,"dollars")
 
 #Subtract costs from income
 
-balence_remaining= int(income)-total
-print(f"You have a remaining balence of",balence_remaining,"dollars this month!")
- 
+balance_remaining= int(income)-total
+print(f"You have a remaining balence of",balance_remaining,"dollars this month!")
